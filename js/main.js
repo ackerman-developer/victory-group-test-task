d5c1e9dbcd5305e22da0a1c00affa5160b801fdd
Artur Imankulov <arturimankulov906@gmail.com>
2024-03-13 22:18:54 +0500

    initial init

---
+++ js/main.js
@@ -0,0 +1,9 @@
+var swiper = new Swiper(".services_slider", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    loop: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
